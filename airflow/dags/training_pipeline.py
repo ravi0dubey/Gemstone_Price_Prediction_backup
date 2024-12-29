47a54288f018f3e85d27fe532816e1d51c2cc66a
ravi0dubey <ravi0dubey@gmail.com>
2024-12-29 17:22:36 -0500

    updating aiflow training_pipeline

--- airflow/dags/training_pipeline.py
+++ airflow/dags/training_pipeline.py
@@ -5,29 +5,62 @@ from textwrap import dedent
 import os
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from source.pipeline.training_pipeline import start_training_pipeline
+from source.pipeline.training_pipeline import Training_Pipeline
 import pendulum
 
+training_pipeline = Training_Pipeline()
 
 with DAG(
     'Gemstone_training',
     default_args={'retries': 2},
     description='Gemstone Prediction',
     schedule_interval="@weekly",
-    start_date=pendulum.datetime(2022, 12, 11, tz="UTC"),
+    start_date=pendulum.datetime(2024, 12, 29, tz="UTC"),
     catchup=False,
-    tags=['example'],
+    tags=['machine learning',"gemstone"],
 ) as dag:
 
-    
-    def training(**kwargs):
-        
-        start_training_pipeline()
-    
+    dag.doc_md = __doc__
 
-    training_pipeline  = PythonOperator(
-            task_id="train_pipeline",
-            python_callable=training
+    def data_ingestion(**kwargs):
+        ti = kwargs["ti"]
+        train_data_path,test_data_path=training_pipeline.start_data_ingestion()
+        ti.xcom_push("data_ingestion_artifact",{"train_data_path":train_data_path},{"test_data_path" : test_data_path})
+    
+    def data_transformation(**kwargs):
+        ti = kwargs["ti"]
+        train_data_path,test_data_path=ti.xcom_pull(task_ids="data_ingestion")
+        train_arr_path,test_arr_path=training_pipeline.start_data_transformation(train_data_path,test_data_path)
+        ti.xcom_push("data_transformation_artifact",{"train_arr_path":train_arr_path},{"test_arr_path" : test_arr_path})
+                          
+    def model_training(**kwargs):
+        ti = kwargs["ti"]
+        train_arr_path,test_arr_path=ti.xcom_pull(task_ids="data_transformation")
+        training_pipeline.start_model_training(train_arr_path,test_arr_path)
+
+    def model_evaluation(**kwargs):
+        ti = kwargs["ti"]
+        train_arr_path,test_arr_path=ti.xcom_pull(task_ids="data_transformation")
+        training_pipeline.start_model_evaluation(train_arr_path,test_arr_path)
+    
+    data_ingestion_pipeline  = PythonOperator(
+            task_id="data_ingestion",
+            python_callable=data_ingestion
+    )
+    
+    data_transformation_pipeline  = PythonOperator(
+            task_id="data_transformation",
+            python_callable=data_ingestion
+    )
+    
+    model_training_pipeline  = PythonOperator(
+            task_id="model_training",
+            python_callable=model_training
     )
 
-    training_pipeline
+    model_evaluation_pipeline  = PythonOperator(
+            task_id="model_evaluation",
+            python_callable=model_training
+    )
+
+    data_ingestion_pipeline>>data_transformation_pipeline>>training_pipeline>>model_evaluation_pipeline
--- source/pipeline/training_pipeline.py
+++ source/pipeline/training_pipeline.py
@@ -9,17 +9,44 @@ from source.components.data_transformation import DataTransformation
 from source.components.model_trainer import ModelTrainer
 from source.components.model_evaluation import ModelEvaluation
 
-def start_training_pipeline():
-    obj=DataIngestion()
-    train_data_path,test_data_path=obj.initiate_data_ingestion()
-    data_transformation=DataTransformation()
-    train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
+class Training_Pipeline:
+    def start_data_ingestion(self):
+        try:
+            data_ingestion_obj=DataIngestion()
+            train_data_path,test_data_path=data_ingestion_obj.initiate_data_ingestion()
+            return train_data_path,test_data_path
+        except Exception as e:
+            raise customexception(e)
+    
+    def start_data_transformation(self,train_data_path,test_data_path):
+        try:
+            data_transformation=DataTransformation()
+            train_arr_path,test_arr_path=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
+            return train_arr_path,test_arr_path
+        except Exception as e:
+            raise customexception(e)
 
-    model_trainer_obj=ModelTrainer()
-    model_trainer_obj.initate_model_training(train_arr,test_arr)
-    # added
-    model_eval_obj = ModelEvaluation()
-    model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
+    def start_model_training(self,train_arr_path,test_arr_path):
+        try:
+            model_trainer_obj=ModelTrainer()
+            model_trainer_obj.initate_model_training(train_arr_path,test_arr_path)
+            return model_trainer_obj
+        except Exception as e:
+            raise customexception(e)
+        
+    def start_model_evaluation(self,train_arr_path,test_arr_path):
+        try:
+            model_eval_obj = ModelEvaluation()
+            model_eval_obj.initiate_model_evaluation(train_arr_path,test_arr_path)
+        except Exception as e:
+            raise customexception(e)
+        
+    def start_training_pipeline(self):
+        train_data_path,test_data_path= self.start_data_ingestion()
+        train_arr_path,test_arr_path= self.start_data_transformation(train_data_path,test_data_path)
+        self.start_model_training(train_arr_path,test_arr_path)
+        self.start_model_evaluation(train_arr_path,test_arr_path)
 
 if __name__=="__main__":
-    start_training_pipeline()
+    training_object = Training_Pipeline()
+    training_object.start_training_pipeline()
